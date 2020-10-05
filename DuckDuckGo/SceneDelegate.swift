d28bcd9b3b6d37ee69fa83d8cc369b5deba01e9d
Meir Radnovich <meir@radnovich.com>
2020-10-05 23:17:25 +0300

    Starting multiple window support.

---
+++ DuckDuckGo/SceneDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SceneDelegate.swift
+//  DuckDuckGo
+//
+//  Created by Meir Radnovich on 17 Tishri 5781.
+//  Copyright © 5781 DuckDuckGo. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate : NSObject, UIWindowSceneDelegate {
+    
+}
